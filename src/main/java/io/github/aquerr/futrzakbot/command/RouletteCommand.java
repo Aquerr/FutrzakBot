70d1d3ee9f6dcf133c9535f4acf3390104662111
Aquerr <nerdianin@gmail.com>
2022-09-20 11:22:41 +0200

    Add Roulette Slash Command

--- src/main/java/io/github/aquerr/futrzakbot/command/RouletteCommand.java
+++ src/main/java/io/github/aquerr/futrzakbot/command/RouletteCommand.java
@@ -4,21 +4,35 @@ import io.github.aquerr.futrzakbot.command.context.CommandContext;
 import io.github.aquerr.futrzakbot.games.RouletteGame;
 import net.dv8tion.jda.api.entities.Member;
 import net.dv8tion.jda.api.entities.TextChannel;
+import net.dv8tion.jda.api.events.interaction.ButtonClickEvent;
+import net.dv8tion.jda.api.events.interaction.SlashCommandEvent;
+import net.dv8tion.jda.api.interactions.commands.build.CommandData;
 
 import java.util.Collections;
 import java.util.List;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
 
-public class RouletteCommand implements Command
+public class RouletteCommand implements Command, SlashCommand
 {
+    private static final ScheduledExecutorService SCHEDULER = Executors.newScheduledThreadPool(10);
+
     @Override
     public boolean execute(CommandContext context)
     {
         TextChannel channel = context.getTextChannel();
         Member member = context.getMember();
+        useRoulette(channel, member);
+        return true;
+    }
+
+    private void useRoulette(TextChannel channel, Member member)
+    {
         long guildId = channel.getGuild().getIdLong();
         if (!RouletteGame.isActive(guildId))
         {
-            channel.sendMessage(member.getAsMention()).append(" rozpoczyna nową grę w ruletkę!").complete();
+            channel.sendMessage(member.getAsMention() + " rozpoczyna nową grę w ruletkę!").complete();
             channel.sendMessage("Będzie gorąco!").complete();
             RouletteGame.startNewGame(guildId);
         }
@@ -27,18 +41,20 @@ public class RouletteCommand implements Command
 
         if (killed)
         {
-            channel.sendMessage(member.getAsMention()).append(" pociąga za spust!").complete();
-            channel.sendMessage("STRZAŁ!").complete();
-            channel.sendMessage(member.getAsMention()).append(" jest już w innym świecie :') ").complete();
-            channel.getGuild().mute(member, true).reason("Mutuję Cię na 30sekund!").complete();
+            channel.sendMessage(member.getAsMention() + " pociąga za spust!").complete();
+            channel.sendMessage("STRZAŁ! :boom:").complete();
+            channel.sendMessage(member.getAsMention() + " jest już w innym świecie :skull_crossbones: (mute na 30 sek)").complete();
+            channel.getGuild().mute(member, true).reason("Mutuję Cię na 30 sekund z powodu śmierci w ruletce! :)").complete();
+            SCHEDULER.schedule(() -> {
+                channel.getGuild().mute(member, false).reason("Zostałeś wskrzeszony! :)").complete();
+            }, 30, TimeUnit.SECONDS);
         }
         else
         {
-            channel.sendMessage(member.getAsMention()).append(" pociąga za spust!").complete();
+            channel.sendMessage(member.getAsMention() + " pociąga za spust!").complete();
             channel.sendMessage("Z pistoletu słychać tylko odgłos kliknięcia!").complete();
-            channel.sendMessage(member.getAsMention()).append(" udało się przeżyć ruletkę.").complete();
+            channel.sendMessage(member.getAsMention() + " udało się przeżyć ruletkę. :sunglasses: ").complete();
         }
-        return true;
     }
 
     @Override
@@ -57,5 +73,37 @@ public class RouletteCommand implements Command
     public String getDescription()
     {
         return "Rosyjska ruletka";
+    }
+
+    @Override
+    public CommandData getSlashCommandData()
+    {
+        return new CommandData(getAliases().get(0), getDescription())
+                .setDefaultEnabled(true);
+    }
+
+    @Override
+    public void onSlashCommand(SlashCommandEvent event)
+    {
+        event.reply("Zagrajmy więc!").queue();
+        useRoulette(event.getTextChannel(), event.getMember());
+    }
+
+    @Override
+    public void onButtonClick(ButtonClickEvent event)
+    {
+
+    }
+
+    @Override
+    public boolean supports(SlashCommandEvent event)
+    {
+        return event.getName().equals(getAliases().get(0));
+    }
+
+    @Override
+    public boolean supports(ButtonClickEvent event)
+    {
+        return false;
     }
 }
