a210196a16ab04414ad4acd8190ad86084029374
Aquerr <nerdianin@gmail.com>
2022-11-09 19:05:56 +0100

    Add Skip Command

--- src/main/java/io/github/aquerr/futrzakbot/command/SkipCommand.java
+++ src/main/java/io/github/aquerr/futrzakbot/command/SkipCommand.java
@@ -3,11 +3,12 @@ package io.github.aquerr.futrzakbot.command;
 import io.github.aquerr.futrzakbot.audio.FutrzakAudioPlayerManager;
 import io.github.aquerr.futrzakbot.command.context.CommandContext;
 import net.dv8tion.jda.api.entities.TextChannel;
+import net.dv8tion.jda.api.events.interaction.SlashCommandEvent;
 
 import java.util.Collections;
 import java.util.List;
 
-public class SkipCommand implements Command
+public class SkipCommand implements Command, SlashCommand
 {
     private final FutrzakAudioPlayerManager futrzakAudioPlayerManager;
 
@@ -40,5 +41,13 @@ public class SkipCommand implements Command
     public String getDescription()
     {
         return "Pomiń utwór";
+    }
+
+    @Override
+    public void onSlashCommand(SlashCommandEvent event)
+    {
+        event.reply("Skipping track...").queue();
+        TextChannel textChannel = event.getTextChannel();
+        this.futrzakAudioPlayerManager.skipAndPlayNextTrack(textChannel.getGuild().getIdLong(), textChannel);
     }
 }
